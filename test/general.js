6a3367099abf3443357f82f1684d7cac4b471cb9
crystalbit <crystalbit@mail.ru>
2020-04-13 22:44:51 +0300

    add tests - increase code coverage

--- test/general.js
+++ test/general.js
@@ -2,10 +2,23 @@ const expect = require("chai").expect;
 const Planner = require('../');
 
 const INTERVAL = 50;
+const SPEEDUP_ERROR_MESSAGE = 'Speedup can\'t be 0 if threshold isn\'t';
 
 describe("General Test", () => {
-    it(`throws error if threshold over 0 and speed up eq to 0`, () => {
-        expect(() => new Planner(INTERVAL, 1000, 0)).to.throw('Speedup can\'t be 0 if threshold isn\'t');
+    it(`throws error if threshold over 0 and speedup is 0`, () => {
+        expect(() => new Planner(INTERVAL, 1000, 0)).to.throw(SPEEDUP_ERROR_MESSAGE);
+    });
+
+    it(`no error if threshold is 0 and speedup is 0`, () => {
+        expect(() => new Planner(INTERVAL, 0, 0)).not.to.throw(SPEEDUP_ERROR_MESSAGE);
+    });
+
+    it(`no error if threshold is 0 and speedup isn't 0`, () => {
+        expect(() => new Planner(INTERVAL, 0, 2)).not.to.throw(SPEEDUP_ERROR_MESSAGE);
+    });
+
+    it(`no error if threshold isn't 0 and speedup isn't 0`, () => {
+        expect(() => new Planner(INTERVAL, 3000, 2)).not.to.throw(SPEEDUP_ERROR_MESSAGE);
     });
 
     it(`check return types`, () => {
